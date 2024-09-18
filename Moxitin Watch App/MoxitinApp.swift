d8fead2159a3010363f4091a23d08a52cde37738
Christian Ray Leovido <18484997+leovido@users.noreply.github.com>
2024-09-18 13:28:17 +0100

    feat: new logo

--- Moxitin Watch App/MoxitinApp.swift
+++ Moxitin Watch App/MoxitinApp.swift
@@ -11,36 +11,36 @@ import MoxieLib
 @main
 struct Moxitin_Watch_AppApp: App {
 	@State private var entry: SimpleEntry = .init(date: .now, dailyMoxie: 2039409, dailyUSD: 394, claimableMoxie: 30493, claimableUSD: 39402934, claimedMoxie: 4930293)
-    var body: some Scene {
-        WindowGroup {
-					ContentView(entry: entry)
-						.onAppear {
-							do {
-								var moxieModel: MoxieModel = .noop
-
-								if let userDefaults = UserDefaults(suiteName: "group.com.christianleovido.moxito"),
-									 let data = userDefaults.data(forKey: "moxieData"),
-									 let decodedModel = try? CustomDecoderAndEncoder.decoder.decode(
-										MoxieModel.self,
-										from: data
-									 ) {
-									moxieModel = decodedModel
-								}
-								
-//								let price = try await configuration.client.fetchPrice()
-								
-								self.entry = SimpleEntry(date: .now,
-																							dailyMoxie: moxieModel.allEarningsAmount,
-																				dailyUSD:  moxieModel.allEarningsAmount,
-																				claimableMoxie: moxieModel.moxieClaimTotals.first!.availableClaimAmount,
-																				claimableUSD: moxieModel.moxieClaimTotals.first!.availableClaimAmount,
-																				claimedMoxie: moxieModel.moxieClaimTotals.first!.claimedAmount)
-								
-							} catch {
-								dump(error)
-							}
+	var body: some Scene {
+		WindowGroup {
+			ContentView(entry: entry)
+				.onAppear {
+					do {
+						var moxieModel: MoxieModel = .noop
+						
+						if let userDefaults = UserDefaults(suiteName: "group.com.christianleovido.moxito"),
+							 let data = userDefaults.data(forKey: "moxieData"),
+							 let decodedModel = try? CustomDecoderAndEncoder.decoder.decode(
+								MoxieModel.self,
+								from: data
+							 ) {
+							moxieModel = decodedModel
 						}
-        }
-    }
-		
+						
+						//								let price = try await configuration.client.fetchPrice()
+						
+						self.entry = SimpleEntry(date: .now,
+																		 dailyMoxie: moxieModel.allEarningsAmount,
+																		 dailyUSD:  moxieModel.allEarningsAmount,
+																		 claimableMoxie: moxieModel.moxieClaimTotals.first!.availableClaimAmount,
+																		 claimableUSD: moxieModel.moxieClaimTotals.first!.availableClaimAmount,
+																		 claimedMoxie: moxieModel.moxieClaimTotals.first!.claimedAmount)
+						
+					} catch {
+						dump(error)
+					}
+				}
+		}
+	}
+	
 }
